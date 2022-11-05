78c5a03c09f79d5c850b12ea8d73952289843b1e
Nicholas Catalan <107813010+Catalan-Nick@users.noreply.github.com>
2022-11-04 20:10:48 -0500

    contact page

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-var navbar = 
-        document.querySelector(".navbar");
-  
-    navbar.addEventListener(
+var navbar = document.querySelector(".color-change");
+    document.body.addEventListener(
         "mousemove", function (e) {
+
         x = e.offsetX;
+        if (x > 255){
+            x -= 255
+        }
         y = e.offsetY;
+        if (y > 255){
+            y -= 255
+        }
         navbar.style.backgroundColor 
-            = `rgb(${x}, ${y}, ${x - y})`;
+            = `rgb(${x}, ${y}, ${x})`;
         });
--- nav.js
+++ nav.js
@@ -1,36 +1,37 @@
 $(function () {
     var bar = '';
-    bar += '<nav class="navbar navbar-expand-lg bg-dark ">';
-    bar += '<div class="container-fluid ">';
-    bar += '<a class="navbar-brand text-light bg-success rounded-pill" href="index.html">NC</a>'
+    bar += '<div class="container-fluid">';
+    bar += '<a class="navbar-brand  rounded-2" href="index.html">'
+    bar += '<img src="Images/Logo.png" alt="NC logo" class="logo">'
+    bar += '</a>'
     bar += '<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">'
     bar += '<span class="navbar-toggler-icon"></span>'
     bar += '</button>'
     bar += '<div class="collapse navbar-collapse" id="navbarSupportedContent">'
     bar += '<ul class="navbar-nav me-auto mb-2 mb-lg-0 ">'
     bar += '<li class="nav-item">'
-    bar += '<a class="nav-link active text-light" aria-current="page" href="index.html">Home</a>'
+    bar += '<a class="nav-link active text-outline text-light" aria-current="page" href="index.html">Home</a>'
     bar += '</li>'
     bar += '<li class="nav-item">'
-    bar += '<a class="nav-link text-light" href="about.html">Contact</a>'
+    bar += '<a class="nav-link text-outline text-light" href="about.html">Contact</a>'
     bar += '</li>'
     bar += '<li class="nav-item dropdown">'
-    bar += '<a class="nav-link text-light dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Projects'
+    bar += '<a class="nav-link text-outline text-light dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Projects'
     bar += '</a>'
     bar += '<ul class="dropdown-menu bg-dark">'
-    bar += '<li><a class="dropdown-item text-light" href="#">Project Previews</a></li>'
+    bar += '<li><a class="dropdown-item text-outline text-light" href="projects.html">Project Previews</a></li>'
     bar += '<li><hr class="dropdown-divider bg-white"></li>'
-    bar += '<li><a class="dropdown-item text-light" href="#">Another action</a></li>'
-    bar += '<li><a class="dropdown-item text-light" href="#">Something else here</a></li>'
+    bar += '<li><a class="dropdown-item text-outline text-light" href="#">Another action</a></li>'
+    bar += '<li><a class="dropdown-item text-outline text-light" href="#">Something else here</a></li>'
     bar += '</ul>'
     bar += '</li>'
     bar += '<li class="nav-item">'
-    bar += '<a class="nav-link text-light" href="about.html">Coding Challenges</a>'
+    bar += '<a class="nav-link text-outline text-light" href="about.html">Coding Challenges</a>'
     bar += '</li>'
     bar += ' </ul>'
     bar += '</div>'
     bar += '</div>'
-    bar += '</nav>'
+    
 
     $("#main-bar").html(bar);
 
